0949b58048188a1384ffb829d805a159b86e9037
PRafull Gupta <gprafullkuma@vmware.com>
2020-04-04 14:17:17 +0530

    Move zeroes to end of array in place.

---
+++ LeetCode30DaysChallenge/src/MoveZeros.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ * <p>
+ * Example:
+ * <p>
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ * Note:
+ * <p>
+ * You must do this in-place without making a copy of the array.
+ * Minimize the total number of operations.
+ */
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 0, 3, 12};
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void moveZeroes(int[] nums) {
+
+        int i = 0;
+        int index = 0;
+        while (i < nums.length) {
+            if (nums[i] != 0) {
+                nums[index++] = nums[i];
+            }
+            i++;
+        }
+
+        while (index < nums.length) {
+            nums[index++] = 0;
+        }
+    }
+}
