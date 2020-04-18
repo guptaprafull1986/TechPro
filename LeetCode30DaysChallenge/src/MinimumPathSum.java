5a275a00cf4c961a7f3558324331b2a03eee99a2
PRafull Gupta <gprafullkuma@vmware.com>
2020-04-18 17:29:51 +0530

    MinimumPathSum

---
+++ LeetCode30DaysChallenge/src/MinimumPathSum.java
@@ -0,0 +1,45 @@
+/**
+ * Given a m x n grid filled with non-negative numbers,
+ * find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+ * <p>
+ * Note: You can only move either down or right at any point in time.
+ * <p>
+ * Example:
+ * <p>
+ * Input:
+ * [
+ * [1,3,1],
+ * [1,5,1],
+ * [4,2,1]
+ * ]
+ * Output: 7
+ * Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+ */
+
+public class MinimumPathSum {
+    public static void main(String[] args) {
+
+    }
+
+    public static int minPathSum(int[][] grid) {
+        int r = grid.length;
+        int c = grid[0].length;
+        int[][] dp = new int[r][c];
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                if (i == 0 && j == 0) {
+                    dp[i][j] = grid[i][j];
+                } else if (i == 0) {
+                    dp[i][j] = dp[i][j - 1] + grid[i][j];
+                } else if (j == 0) {
+                    dp[i][j] = dp[i - 1][j] + grid[i][j];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                }
+            }
+        }
+
+        return dp[r - 1][c - 1];
+    }
+}
